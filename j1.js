a51eeb3facfd71673702757f715a73c0e30e0c50
preksha <jainpreksha2712@gmail.com>
2021-12-08 16:04:18 +0530

    new javascript

---
+++ j1.js
@@ -0,0 +1,11 @@
+// add new html and change existing html from dom
+// react to event
+    // 1. response from server
+    //2.key press
+    // 3.mouse movement
+// ajax request
+// get and set cookies and use local storage
+
+// what cant in browser javascript do?
+/*1. read and write to and from computer hard disk
+2. same origin policy*/ 
